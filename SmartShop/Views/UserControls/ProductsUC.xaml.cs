d93381e510ebff2862d879d00b233fd2b42c5a1b
xuannhungg <hungsam37@gmail.com>
2023-05-11 22:39:58 +0700

    #3 add products view

--- SmartShop/Views/UserControls/ProductsUC.xaml.cs
+++ SmartShop/Views/UserControls/ProductsUC.xaml.cs
@@ -26,7 +26,20 @@ namespace SmartShop.Views.UserControls
         public ProductsUC()
         {
             InitializeComponent();
+            var products = GetProducts();
+            if (products.Count > 0)
+                ListViewProducts.ItemsSource = products;
         }
+        private List<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product("Product 1",205,"1.jpg"),
+                new Product("tât chân tất tay may mặc",206,"E:/Lập trình window/WpfApp1/WpfApp1/2.jpg"),
+                new Product("Product 3",207,"E:/Lập trình window/WpfApp1/WpfApp1/3.jpg"),
+                new Product("Product 4",208,"E:/Lập trình window/WpfApp1/WpfApp1/4.jpg")
 
+            };
+        }
     }
 }
