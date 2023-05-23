820b2fdcdc9fa6291624b8426bfbc45ee9dd36ac
AnhAnNek <info.vananne@gmail.com>
2023-05-23 09:53:44 +0700

    rebase develop

--- SmartShop/ViewModels/UserControls/ProductsViewModel.cs
+++ SmartShop/ViewModels/UserControls/ProductsViewModel.cs
@@ -1,14 +1,43 @@
 ﻿using SmartShop.ViewModels.Base;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using SmartShop.Models;
 
 namespace SmartShop.ViewModels.UserControls
 {
     public class ProductsViewModel : BaseViewModel
     {
+        private List<Product> products;
+        public List<Product> Products { get => products; set { products = value; OnPropertyChanged(); } }
+        
+        public ICommand AddToCartCommand { get; private set; }
 
+        public ProductsViewModel()
+        {
+            LoadProducts();
+            SetCommands();
+        }
+
+        private void LoadProducts()
+        {
+            Products = new List<Product>()
+            {
+                new Product { ID = "123123" },
+                new Product { ID = "123124" },
+                new Product { ID = "123125" },
+                new Product { ID = "123126" }
+            };
+        }
+
+        private void SetCommands()
+        {
+            AddToCartCommand = new RelayCommand<Product>(ExecuteAddToCard);
+        }
+
+        private void ExecuteAddToCard(Product prod)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- SmartShop/Views/UserControls/ProductsUC.xaml.cs
+++ SmartShop/Views/UserControls/ProductsUC.xaml.cs
@@ -1,20 +1,4 @@
-﻿using SmartShop.Models;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartShop.Views.UserControls
 {
@@ -26,20 +10,6 @@ namespace SmartShop.Views.UserControls
         public ProductsUC()
         {
             InitializeComponent();
-            var products = GetProducts();
-            if (products.Count > 0)
-                ListViewProducts.ItemsSource = products;
-        }
-        private List<Product> GetProducts()
-        {
-            return new List<Product>()
-            {
-                // new Product("Product 1",205,"1.jpg"),
-                // new Product("tât chân tất tay may mặc",206,"E:/Lập trình window/WpfApp1/WpfApp1/2.jpg"),
-                // new Product("Product 3",207,"E:/Lập trình window/WpfApp1/WpfApp1/3.jpg"),
-                // new Product("Product 4",208,"E:/Lập trình window/WpfApp1/WpfApp1/4.jpg")
-
-            };
         }
     }
 }
