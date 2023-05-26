b07a9bc9e72f8145955987da37c39bd5893b2be5
xuannhungg <hungsam37@gmail.com>
2023-05-26 11:21:41 +0700

    #1 convert base64 to bitmap

---
+++ SmartShop/Converters/Base64Converter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace SmartShop.Converters
+{
+    public class Base64Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string base64Image)
+            {
+                byte[] imageBytes = System.Convert.FromBase64String(base64Image);
+
+                using (MemoryStream memoryStream = new MemoryStream(imageBytes))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.StreamSource = memoryStream;
+                    bitmapImage.EndInit();
+                    bitmapImage.Freeze(); // Optional optimization to freeze the image and improve performance
+
+                    return bitmapImage;
+                }
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
